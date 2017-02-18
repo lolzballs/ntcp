bcf66ac1c61859adb48bea7f6ae10fa13a56a34c
Benjamin Cheng <ben@bcheng.cf>
2017-02-18 10:21:06 -0500

    Tunnel client

--- examples/tunnel_client/src/main.rs
+++ examples/tunnel_client/src/main.rs
@@ -1,10 +1,66 @@
 extern crate ntcp;
 
+use std::collections::HashMap;
+use std::net::UdpSocket;
+use std::sync::mpsc;
+use std::thread;
+
 use ntcp::ipv4;
 use ntcp::platform::RawSocket;
-use ntcp::socket::{Interface, Socket};
+use ntcp::socket::{PacketBuffer, SocketInterface, Socket};
 use ntcp::tcp;
 
-fn create_client() {}
+fn create_client(tx: mpsc::Sender<Vec<u8>>, rx: mpsc::Receiver<Vec<u8>>) {
+    let raw = RawSocket::new().unwrap();
+    let endpoint = tcp::Endpoint::new(ipv4::Address::default(), 6969);
+    let mut interface = SocketInterface::new(endpoint, raw);
 
-fn main() {}
+    let mut socket =
+        interface.connect(tcp::Endpoint::new(ipv4::Address::from_bytes(&[127, 0, 0, 1]), 6969))
+            .unwrap();
+
+    let endpoint = socket.endpoint;
+    let (socket_tx, socket_rx) = socket.to_tx_rx();
+
+    thread::spawn(move || {
+        loop {
+            let packet = rx.recv().unwrap();
+            socket_tx.send((endpoint, PacketBuffer::new(&packet))).unwrap();
+        }
+    });
+    thread::spawn(move || {
+        loop {
+            let packet = socket_rx.recv().unwrap();
+            tx.send((*packet.payload).to_vec());
+        }
+    });
+}
+
+fn main() {
+    let socket = UdpSocket::bind("127.0.0.1:25566").unwrap();
+    let mut sockets = HashMap::new();
+    loop {
+        let mut buf = [0; 1024];
+        let (len, src) = socket.recv_from(&mut buf).unwrap();
+
+        let tx = sockets.entry(src).or_insert_with(|| {
+            let (send_tx, send_rx) = mpsc::channel();
+            let (recv_tx, recv_rx) = mpsc::channel();
+
+            create_client(send_tx, recv_rx);
+
+            {
+                let socket = socket.try_clone().unwrap();
+                thread::spawn(move || {
+                    loop {
+                        let packet = send_rx.recv().unwrap();
+                        socket.send_to(&packet, src).unwrap();
+                    }
+                });
+            }
+            recv_tx
+        });
+
+        tx.send(buf[..len].to_vec()).unwrap();
+    }
+}
